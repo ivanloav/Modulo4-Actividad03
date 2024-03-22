49b41202793add7c7c084536747feca17ef2a5cd
ivanloav <ivanloav@gmail.com>
2024-03-22 23:39:27 +0100

    Lista de tareas login

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,71 +1,74 @@
-import './loginform.css';                      // Importa los estilos del componente LoginForm 
-import { useState, useEffect } from 'react';   // Importa useState y useEffect de React
+import './loginform.css';                     // Importa los estilos del componente LoginForm
+import { useState, useEffect } from 'react';  // Importa los hooks useState y useEffect de React
 
 export function LoginForm({ onLoginSuccess }) {
-  const [rememberMe, setRememberMe] = useState(false);                                // Estado para recordar las credenciales
-  const [error, setError] = useState(false);                                          // Estado para mostrar un error visual
-  const [credentials, setCredentials] = useState({ email: '', password: '' });        // Estado para las credenciales
-  const [isLoading, setIsLoading] = useState(false);                                  // Estado para mostrar el indicador de carga
+  // Definición de estados
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState(false);
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const [isLoading, setIsLoading] = useState(false);
 
-  const setEmail = (email) => setCredentials(prev => ({ ...prev, email }));           // Función para actualizar el email
-  const setPassword = (password) => setCredentials(prev => ({ ...prev, password }));  // Función para actualizar la contraseña
+  // Definición de funciones
+  const setEmail = (email) => setCredentials(prev => ({ ...prev, email }));
+  const setPassword = (password) => setCredentials(prev => ({ ...prev, password }));
 
-  // Función para cargar las credenciales almacenadas en localStorage al montar el componente
+  // Efecto para cargar las credenciales almacenadas en localStorage
   useEffect(() => {
-    const storedCredentials = localStorage.getItem('credentials'); // Obtiene las credenciales almacenadas en localStorage
-    // Si hay credenciales almacenadas, las carga en el estado
+    const storedCredentials = localStorage.getItem('credentials');
+    
     if (storedCredentials) {
-      const parsedCredentials = JSON.parse(storedCredentials);                                      // Parsea las credenciales almacenadas
-      setCredentials({ email: parsedCredentials.username, password: parsedCredentials.password });  // Actualiza el estado con las credenciales almacenadas
-      setRememberMe(true);                                                                          // Marca el checkbox de recordar credenciales
+      const parsedCredentials = JSON.parse(storedCredentials);
+      setCredentials({ email: parsedCredentials.username, password: parsedCredentials.password });
+      setRememberMe(true);
     }
   }, []);
 
   // Función para manejar el envío del formulario
   const handleSubmit = async (event) => {
-    event.preventDefault(); // Previene el comportamiento por defecto del formulario
-    setIsLoading(true);     // Comienza a mostrar el indicador de carga
+    event.preventDefault();
+    setIsLoading(true);
 
-    // Función asíncrona para simular el inicio de sesión con una espera de 1 segundo
+    // Función para simular un tiempo de espera
     setTimeout(async () => {
-      // Si el checkbox de recordar credenciales está marcado, guarda las credenciales en localStorage si no esta marcado, las elimina
+
+      // Almacenar las credenciales en localStorage si se ha marcado el checkbox o eliminarlas si no
       if (rememberMe) {
         localStorage.setItem('credentials', JSON.stringify({ username: credentials.email, password: credentials.password }));
       } else {
         localStorage.removeItem('credentials');
       }
-      // Actualiza el estado para no mostrar un error visual
+      
       setError(false);
 
-      // Intenta iniciar sesión con las credenciales proporcionadas
+      // Realizar la petición de inicio de sesión
       try {
-        // Realiza una petición POST a la API de ReqRes para iniciar sesión
+        // Configurar las opciones de la petición
         const requestOptions = {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email: credentials.email, password: credentials.password })
         };
-        // Espera la respuesta de la petición
+        
+        // Realizar la petición a la API
         const response = await fetch('https://reqres.in/api/login', requestOptions);
         const data = await response.json();
         
-        // Si la respuesta no es exitosa, lanza un error con el mensaje de error de la API
         if (!response.ok) {
           throw new Error(data.error);
         }
         
-        onLoginSuccess();                                   // Llama a onLoginSuccess si el inicio de sesión es exitoso
-      } catch (err) {                                       // Captura cualquier error que ocurra durante el inicio de sesión
-        setError(true);                                     // Actualiza el estado para mostrar un error visual
-        alert('Error de inicio de sesión: ' + err.message); // Muestra un mensaje de alerta con el error
+        onLoginSuccess();
+      } catch (err) {
+        setError(true);
+        alert('Error de inicio de sesión: ' + err.message);
       }
       
-      setIsLoading(false);                                  // Oculta el indicador de carga
-    }, 1000);                                               // Espera 1 segundo antes de ejecutar la lógica de inicio de sesión
+      setIsLoading(false);
+    }, 1000);
   };
 
   // Renderiza el componente
-  return (
+  return (                                                                                   
     <div>
       {isLoading && (
         <div className="loadingOverlay">
